648ac6a537de69eafe61895aab2956ce4e17084b
Aeium <nate2@umbc.edu>
2016-08-22 17:15:16 -0400

    printing frame oss

--- player_oss.c
+++ player_oss.c
@@ -8,45 +8,22 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/ioctl.h>
-//#include <linux/soundcard.h>
-
-#define ALSA_PCM_NEW_HW_PARAMS_API
-
-#include <alsa/asoundlib.h>
+#include <linux/soundcard.h>
 
 size_t strlen(const char *s);
 #define out(text) write(1, (const void *) text, strlen(text))
 
 int main(int argc, char *argv[]) {
-    
-	// decoder
-	
-	mp3_decoder_t mp3;
+    mp3_decoder_t mp3;
     mp3_info_t info;
-    int fd; //, pcm;
-
-	// mp3 file
-	
+    int fd, pcm;
     void *file_data;
     unsigned char *stream_pos;
     signed short sample_buf[MP3_MAX_SAMPLES_PER_FRAME];
     int bytes_left;
     int frame_size;
+    int value;
 
-	// alsa init
-	
-	snd_pcm_t *handle;
-	snd_pcm_hw_params_t *params;
-	snd_pcm_uframes_t frames;
-
-	// more alsa init 
-	
-	int rc;
-	unsigned int val;
-	int dir;
-	int size;
-	char *buffer;
-	
     out("minimp3 -- a small MPEG-1 Audio Layer III player based on ffmpeg\n\n");
     if (argc < 2) {
         out("Error: no input file specified!\n");
@@ -60,87 +37,41 @@ int main(int argc, char *argv[]) {
         out("'!\n");
         return 1;
     }
+    
+    bytes_left = lseek(fd, 0, SEEK_END);
 
-    bytes_left = lseek(fd, 0, SEEK_END);    
+    printf("bytes_left: %d \n", bytes_left);
+    
     file_data = mmap(0, bytes_left, PROT_READ, MAP_PRIVATE, fd, 0);
     stream_pos = (unsigned char *) file_data;
     bytes_left -= 100;
-	
-	  /* Open PCM device for playback. */
-
-	  
-  rc = snd_pcm_open(&handle, "default",
-                    SND_PCM_STREAM_PLAYBACK, 0);
-  if (rc < 0) {
-    fprintf(stderr,
-            "unable to open pcm device: %s\n",
-            snd_strerror(rc));
-    exit(1);
-  }
-
-  /* Allocate a hardware parameters object. */
-  snd_pcm_hw_params_alloca(&params);
-
-  /* Fill it in with default values. */
-  snd_pcm_hw_params_any(handle, params);
-
-  /* Set the desired hardware parameters. */
-
-  /* Interleaved mode */
-  snd_pcm_hw_params_set_access(handle, params,
-                      SND_PCM_ACCESS_RW_INTERLEAVED);
-
-  /* Signed 16-bit little-endian format */
-  snd_pcm_hw_params_set_format(handle, params,
-                              SND_PCM_FORMAT_S16_LE);
-
-  /* Two channels (stereo) */
-  snd_pcm_hw_params_set_channels(handle, params, 2);
-
-  /* 44100 bits/second sampling rate (CD quality) */
-  val = 44100;
-  snd_pcm_hw_params_set_rate_near(handle, params,
-                                  &val, &dir);
-
-  /* Set period size to 1 frame. */
-  frames = 1;
-  snd_pcm_hw_params_set_period_size_near(handle,
-                              params, &frames, &dir);
-
-  /* Write the parameters to the driver */
-  rc = snd_pcm_hw_params(handle, params);
-  if (rc < 0) {
-    fprintf(stderr,
-            "unable to set hw parameters: %s\n",
-            snd_strerror(rc));
-    exit(1);
-  }
-
-  /* Use a buffer large enough to hold one period */
-  snd_pcm_hw_params_get_period_size(params, &frames,
-                                    &dir);
-  size = frames * 4; /* 2 bytes/sample, 2 channels */
-  //buffer = (char *) malloc(size);
-	
-	
-	out("Now Playing: ");
+    out("Now Playing: ");
     out(argv[1]);
 
+    printf("\nstream_pos: %d \n", stream_pos);
+
+    printf("bytes_left: %d \n", bytes_left);
+
     mp3 = mp3_create();
+
+    if(mp3){
+	printf("mp3 found\n");
+    }
+
     frame_size = mp3_decode(mp3, stream_pos, bytes_left, sample_buf, &info);
     if (!frame_size) {
         out("\nError: not a valid MP3 audio file!\n");
         return 1;
     }
-
-	printf("1");
     
     #define FAIL(msg) { \
         out("\nError: " msg "\n"); \
         return 1; \
     }   
-	/*
-	
+
+    pcm = open("/dev/dsp", O_WRONLY);
+    if (pcm < 0) FAIL("cannot open DSP");
+
     value = AFMT_S16_LE;
     if (ioctl(pcm, SNDCTL_DSP_SETFMT, &value) < 0)
         FAIL("cannot set audio format");
@@ -151,15 +82,12 @@ int main(int argc, char *argv[]) {
     if (ioctl(pcm, SNDCTL_DSP_SPEED, &info.sample_rate) < 0)
         FAIL("cannot set audio sample rate");
 
-	*/
-	
- 
+    out("\n\nPress Ctrl+C to stop playback.\n");
+
 	int once = 1;
 	
-    while (once--){  //(bytes_left >= 0) && (frame_size > 0)) {
-
-	printf("2");
-	printf("stream_pos: %d \n", stream_pos);
+    while (once--){ //(bytes_left >= 0) && (frame_size > 0)) {
+		
 		int i;
 		for( i =0; i < frame_size; i++){
 			
@@ -168,18 +96,13 @@ int main(int argc, char *argv[]) {
 		}
 		
 		printf("\n");
+		
         stream_pos += frame_size;
         bytes_left -= frame_size;
-		snd_pcm_writei(handle, (char*)sample_buf, frames);
-        //write(pcm, (const void *) sample_buf, info.audio_bytes);
+        write(pcm, (const void *) sample_buf, info.audio_bytes);
         frame_size = mp3_decode(mp3, stream_pos, bytes_left, sample_buf, NULL);
     }
 
-    //close(pcm);
-	
-	snd_pcm_drain(handle);
-    snd_pcm_close(handle);
-    free(buffer);
-
+    close(pcm);
     return 0;
 }
